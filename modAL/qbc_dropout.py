3c9ebfa65ecf0e90b02096938f107290267c8445
nibydlo <dp.krylov@gmail.com>
2020-03-27 02:15:43 +0300

    add committee samplings

--- modAL/qbc_dropout.py
+++ modAL/qbc_dropout.py
@@ -1,4 +1,5 @@
 from typing import Union, Tuple
+import math
 
 import numpy as np
 from modAL.utils import multi_argmax
@@ -12,23 +13,99 @@ import scipy.sparse as sp
 from scipy.stats import entropy
 
 
-def dropout_uncertainty(classifier: BaseEstimator, X: modALinput, cmt_size=10, **predict_proba_kwargs) -> np.ndarray:
+def get_least_confidence(predictions):
+    return 1 - np.max(predictions, axis=1)
+
+
+def get_margin(predictions):
+    part = np.partition(-predictions, 1, axis=1)
+    margin = - part[:, 0] + part[:, 1]
+    return margin
+
+
+def get_entropy(predictions):
+    return np.transpose(entropy(np.transpose(predictions)))
+
+
+uncertainty_measure_dict = {
+    'least_confident': get_least_confidence,
+    'margin': get_margin,
+    'entropy': get_entropy
+}
+
+
+def predict_by_committee(classifier: BaseEstimator, X: modALinput, cmt_size=10, **predict_proba_kwargs) -> np.ndarray:
     predictions = []
     for _ in range(cmt_size):
         try:
-            predictions.append(classifier.predict(X, **predict_proba_kwargs))
+            predictions.append(classifier.predict_proba(X, with_dropout=True, **predict_proba_kwargs))
         except NotFittedError:
-            return np.ones(shape=(X.shape[0], ))
+            return np.ones(shape=(X.shape[0],))
 
-    mean_predictions = np.mean(predictions, axis=0)
-    return np.transpose(entropy(np.transpose(mean_predictions)))
+    return predictions
 
 
-def qbc_dropout_strategy(classifier: Union[BaseLearner, BaseCommittee],
-                       X: Union[np.ndarray, sp.csr_matrix],
-                       n_instances: int = 20,
-                       **dropout_uncertainty_kwargs
-                       ) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix]]:
-    uncertainty = dropout_uncertainty(classifier, X, **dropout_uncertainty_kwargs)
+def qbc_uncertainty_sampling(
+        classifier: Union[BaseLearner, BaseCommittee],
+        X: Union[np.ndarray, sp.csr_matrix],
+        n_instances: int = 20,
+        cmt_size: int = 10,
+        uncertainty_measure='entropy',
+        **dropout_uncertainty_kwargs
+) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix, list]]:
+
+    if uncertainty_measure not in uncertainty_measure_dict:
+        raise ValueError('uncertainty measure can be equal only to "least_confident", "margin" or "entropy"')
+
+    committee_predictions = predict_by_committee(
+        classifier=classifier,
+        X=X,
+        cmt_size=cmt_size,
+        **dropout_uncertainty_kwargs
+    )
+    uncertainty = uncertainty_measure_dict[uncertainty_measure](np.mean(committee_predictions, axis=0))
+
     query_idx = multi_argmax(uncertainty, n_instances=n_instances)
-    return query_idx, X[query_idx]
+
+    if isinstance(X, list) and isinstance(X[0], np.ndarray):
+        new_batch = [x[query_idx] for x in X]
+    else:
+        new_batch = X[query_idx]
+
+    return query_idx, new_batch
+
+
+dis_func = np.vectorize(lambda x: x * math.log(x))
+
+
+def bald_sampling(
+        classifier: Union[BaseLearner, BaseCommittee],
+        X: Union[np.ndarray, sp.csr_matrix],
+        n_instances: int = 20,
+        cmt_size: int = 10,
+        uncertainty_measure='entropy',
+        **dropout_uncertainty_kwargs
+) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix, list]]:
+
+    if uncertainty_measure not in uncertainty_measure_dict:
+        raise ValueError('uncertainty measure can be equal only to "least_confident", "margin" or "entropy"')
+
+    committee_predictions = predict_by_committee(
+        classifier=classifier,
+        X=X,
+        cmt_size=cmt_size,
+        **dropout_uncertainty_kwargs
+    )
+    uncertainty = uncertainty_measure_dict[uncertainty_measure](np.mean(committee_predictions, axis=0))
+
+    # 0 for members of committee, -1 for classes
+    disagreement = np.sum(dis_func(committee_predictions), axis=(0, -1)) / cmt_size
+
+    query_idx = multi_argmax(uncertainty + disagreement, n_instances=n_instances)
+
+    if isinstance(X, list) and isinstance(X[0], np.ndarray):
+        new_batch = [x[query_idx] for x in X]
+    else:
+        new_batch = X[query_idx]
+
+    return query_idx, new_batch
