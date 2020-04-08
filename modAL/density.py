4f0fca3caa0bc398923240c6e2b5a7209ee5317c
nibydlo <dp.krylov@gmail.com>
2020-04-08 19:21:40 +0300

    add sud using ball tree

--- modAL/density.py
+++ modAL/density.py
@@ -1,12 +1,17 @@
 """
 Measures for estimating the information density of a given sample.
 """
-from typing import Callable, Union
+from typing import Callable, Union, Tuple
 
 import numpy as np
 from scipy.spatial.distance import cosine, euclidean
+from scipy.stats import entropy
+from sklearn.base import BaseEstimator
 from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.neighbors._ball_tree import BallTree
 
+from modAL.uncertainty import classifier_entropy, classifier_uncertainty, classifier_margin
+from modAL.utils import multi_argmax
 from modAL.utils.data import modALinput
 
 
@@ -54,3 +59,45 @@ def information_density(X: modALinput, metric: Union[str, Callable] = 'euclidean
     similarity_mtx = 1/(1+pairwise_distances(X, X, metric=metric))
 
     return similarity_mtx.mean(axis=1)
+
+
+uncertainty_measure_dict = {
+    'least_confident': classifier_uncertainty,
+    'margin': classifier_margin,
+    'entropy': classifier_entropy
+}
+
+
+def sud(classifier: BaseEstimator,
+        X: modALinput,
+        n_instances: int = 1,
+        k_neighbours: int = 20,
+        uncertainty_measure='entropy',
+        transform=None,
+        **uncertainty_measure_kwargs) -> Tuple[np.ndarray, modALinput]:
+
+    if uncertainty_measure not in uncertainty_measure_dict:
+        raise ValueError('uncertainty measure can be equal only to "least_confident", "margin" or "entropy"')
+
+    uncertainty = uncertainty_measure_dict[uncertainty_measure](classifier, X, **uncertainty_measure_kwargs)
+
+    if transform is not None:
+        X = transform(X)
+
+    ball_tree = BallTree(X, leaf_size=5)
+    # density = []
+    # for i in range(len(X) // 1000 + 1):
+    #     print('i =', i)
+    #     density_batch = \
+    #         [
+    #             np.mean(ball_tree.query(x.reshape(1, -1), k=k_neighbours, return_distance=True)[0]) for x in X[i * 1000 : (i + 1) * 1000]
+    #         ]
+    #     density += density_batch
+    #
+    # density = np.array(density)
+
+    density = np.array([np.mean(ball_tree.query(x.reshape(1, -1), k=k_neighbours, return_distance=True)[0]) for x in X])
+
+    sud_measure = uncertainty * density
+    query_idx = multi_argmax(sud_measure, n_instances=n_instances)
+    return query_idx, np.array([])
