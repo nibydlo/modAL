6d1ffad9a4a3bb985ed02d811fcdf638109007ef
nibydlo <dp.krylov@gmail.com>
2020-04-27 18:37:40 +0300

    update qbc dropout

--- modAL/qbc_dropout.py
+++ modAL/qbc_dropout.py
@@ -78,6 +78,12 @@ def qbc_uncertainty_sampling(
 dis_func = np.vectorize(lambda x: 0 if x == 0 else x * math.log(x))
 
 
+def get_disagreement(committee_predictions, committee_size):
+    # 0 for members of committee, -1 for classes
+    disagreement = np.sum(dis_func(committee_predictions), axis=(0, -1)) / committee_size
+    return disagreement
+
+
 def bald_sampling(
         classifier: Union[BaseLearner, BaseCommittee],
         X: Union[np.ndarray, sp.csr_matrix],
@@ -98,9 +104,84 @@ def bald_sampling(
     )
     uncertainty = uncertainty_measure_dict[uncertainty_measure](np.mean(committee_predictions, axis=0))
 
-    # 0 for members of committee, -1 for classes
-    disagreement = np.sum(dis_func(committee_predictions), axis=(0, -1)) / cmt_size
+    disagreement = get_disagreement(committee_predictions, cmt_size)
+    query_idx = multi_argmax(uncertainty + disagreement, n_instances=n_instances)
 
+    if isinstance(X, list) and isinstance(X[0], np.ndarray):
+        new_batch = [x[query_idx] for x in X]
+    else:
+        new_batch = X[query_idx]
+
+    return query_idx, new_batch
+
+
+def bald_modal_sampling(
+        classifier: Union[BaseLearner, BaseCommittee],
+        X: Union[np.ndarray, sp.csr_matrix],
+        n_instances: int = 20,
+        with_dropout=True,
+        **dropout_uncertainty_kwargs
+) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix, list]]:
+
+    img_predictions = classifier.predict_proba([X[0], np.zeros_like(X[1])], with_dropout=with_dropout)
+    txt_predictions = classifier.predict_proba([np.zeros_like(X[0]), X[1]], with_dropout=with_dropout)
+    both_predictions = classifier.predict_proba(X, with_dropout=with_dropout)
+
+    committee_predictions = [img_predictions, txt_predictions, both_predictions]
+    uncertainty = get_entropy(np.mean(committee_predictions, axis=0))
+    disagreement = get_disagreement(committee_predictions, 3)
+
+    query_idx = multi_argmax(uncertainty + disagreement, n_instances=n_instances)
+
+    if isinstance(X, list) and isinstance(X[0], np.ndarray):
+        new_batch = [x[query_idx] for x in X]
+    else:
+        new_batch = X[query_idx]
+
+    return query_idx, new_batch
+
+
+def bald_trident_sampling(
+    classifier: Union[BaseLearner, BaseCommittee],
+        X: Union[np.ndarray, sp.csr_matrix],
+        n_instances: int = 20,
+        with_dropout=False,
+        cmt_size=1,
+        **dropout_uncertainty_kwargs
+) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix, list]]:
+
+    committee_predictions = []
+    for i in range(cmt_size):
+        common_predictions, img_predictions, txt_predictions = classifier.predict_proba(X, with_dropout=with_dropout)
+        committee_predictions.append(common_predictions)
+        committee_predictions.append(img_predictions)
+        committee_predictions.append(txt_predictions)
+
+    uncertainty = get_entropy(np.mean(committee_predictions, axis=0))
+    disagreement = get_disagreement(committee_predictions, cmt_size * 3)
+    query_idx = multi_argmax(uncertainty + disagreement, n_instances=n_instances)
+
+    if isinstance(X, list) and isinstance(X[0], np.ndarray):
+        new_batch = [x[query_idx] for x in X]
+    else:
+        new_batch = X[query_idx]
+
+    return query_idx, new_batch
+
+
+def bald_trident_based_sampling(
+    classifier: Union[BaseLearner, BaseCommittee],
+        X: Union[np.ndarray, sp.csr_matrix],
+        cmt_size=3,
+        n_instances: int = 20
+) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix, list]]:
+
+    committee_predictions = []
+    for _ in range(cmt_size):
+        committee_predictions.append(classifier.predict_proba(X, with_dropout=True)[0])
+
+    uncertainty = get_entropy(np.mean(committee_predictions, axis=0))
+    disagreement = get_disagreement(committee_predictions, 3)
     query_idx = multi_argmax(uncertainty + disagreement, n_instances=n_instances)
 
     if isinstance(X, list) and isinstance(X[0], np.ndarray):
