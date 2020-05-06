050ade3d0aeda6d47237885f17af21b21c9de8c1
nibydlo <dp.krylov@gmail.com>
2020-05-06 22:39:32 +0300

    add ideal learning loss

--- modAL/learning_loss.py
+++ modAL/learning_loss.py
@@ -1,6 +1,10 @@
 from typing import Union, Tuple
 
 import numpy as np
+
+import torch
+import torch.nn.functional as F
+
 from modAL.utils import multi_argmax
 
 from modAL.models.base import BaseLearner, BaseCommittee
@@ -17,3 +21,16 @@ def learning_loss_strategy(classifier: Union[BaseLearner, BaseCommittee],
     query_idx = multi_argmax(losses, n_instances=n_instances).squeeze(axis=1)
     return query_idx, np.array([])
 
+
+def learning_loss_ideal(classifier: Union[BaseLearner, BaseCommittee],
+                           X: Union[np.ndarray, sp.csr_matrix],
+                           y,
+                           n_instances: int = 20,
+                           **predict_kwargs
+                           ) -> Tuple[np.ndarray, Union[np.ndarray, sp.csr_matrix]]:
+    prediction = torch.tensor(classifier.estimator.predict(X))
+    actual = torch.argmax(torch.tensor(y), dim=1)
+    losses = F.nll_loss(prediction, actual, reduction='none')
+    query_idx = multi_argmax(losses.detach().numpy(), n_instances=n_instances)
+
+    return query_idx, np.array([])
